d31d77e451f52bdb1b6caa10e5372355cde23080
Dasimens <oyomi6547@gmail.com>
2026-02-22 20:18:17 +0000

    slideshow

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// // Navigation
+// const nav = document.getElementById('nav');
+// const menuIcon = document.querySelector('#menu-animation');
+
+// function toggleMenu() {
+//     nav.classList.toggle('active');
+//     menuIcon.classList.toggle('active')
+
+// }
+
+
+// function hideMenu() {
+//     nav.classList.remove('active');
+//     menuIcon.classList.remove('active')
+
+// }
+
+// SlideShow-------------------------------------------------------------------------------
+
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.slide');
+
+function switchImage() {
+  images[currentImageIndex].classList.remove('active');
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  images[currentImageIndex].classList.add('active');
+}
+
+setInterval(switchImage, 5000)
